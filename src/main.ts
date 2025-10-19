15f32658a8acd2ea1cc7c6f13869f9a3d481371c
Hayase Taiki <yasse0218@gmail.com>
2025-10-20 00:47:58 +0900

    トークナイザを導入

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,84 @@
-let program: string = "";
+type TokenKind = "RESERVED" | "NUM" | "EOF";
 
-program = ".intel_syntax noprefix\n";
-program += ".globl main\n";
-program += "\n";
-program += "main:\n";
+type Token = {
+  kind: TokenKind;
+  str: string;
+  val: number;
+  next: Token | null;
+};
 
-const text = await Deno.readTextFile("program");
+const newToken = (
+  kind: TokenKind,
+  cur: Token,
+  str: string,
+): Token => {
+  const token: Token = {
+    kind,
+    str,
+    val: 0,
+    next: null,
+  };
+  cur.next = token;
+  return token;
+};
 
-let i = 0;
-program += `  mov rax, ${text[i]}\n`;
-i++;
-while (i !== text.length) {
-  if (text[i] === "+") {
-    i++;
-    program += `  add rax, ${text[i++]}\n`;
-    continue;
+const isDigit = (char: string): boolean => {
+  return /\d/.test(char);
+};
+
+const error = (text: string) => {
+  console.error(text);
+  Deno.exit(1);
+};
+
+const tokenize = (text: string) => {
+  const head: Token = {
+    kind: "EOF",
+    str: "",
+    val: 0,
+    next: null,
+  };
+  let cur: Token = head;
+
+  let i = 0;
+  while (i !== text.length) {
+    if (text[i] === "+" || text[i] === "-") {
+      cur = newToken("RESERVED", cur, text[i++]);
+      continue;
+    }
+
+    if (isDigit(text[i])) {
+      cur = newToken("NUM", cur, text[i]);
+      const start = i;
+      while (isDigit(text[i])) {
+        i++;
+      }
+      const numStr = text.slice(start, i);
+      cur.val = parseInt(numStr);
+      continue;
+    }
+    error("トークナイズできません");
   }
 
-  if (text[i] === "-") {
-    i++;
-    program += `  sub rax, ${text[i++]}\n`;
-    continue;
-  }
+  newToken("EOF", cur, "");
 
-  console.log(
-    `予期しない文字です: '${text[i]}' (文字コード: ${text.charCodeAt(i)})`,
-  );
-  i++;
-  break;
-}
+  return head.next;
+};
 
-program += "  ret\n";
+const main = async () => {
+  let program: string = "";
+  program = ".intel_syntax noprefix\n";
+  program += ".globl main\n";
+  program += "\n";
+  program += "main:\n";
 
-await Deno.writeTextFile("./dist/out.s", program);
+  const text = await Deno.readTextFile("program");
+
+  // トークナイズする
+  const token = tokenize(text);
+  console.dir(token, { depth: null });
+
+  await Deno.writeTextFile("./dist/out.s", program);
+};
+
+main();
